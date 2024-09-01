028495f2c38ea9df1edfb98177d82b43b1127cad
aui_tuthousend <mraihan286@gmail.com>
2024-09-01 19:43:41 +0700

    feat: article

---
+++ src/components/article/Article.vue
@@ -0,0 +1,26 @@
+<script setup>
+import '../../assets/css/artikel.css';
+import '../../assets/css/style.css';
+import '../../assets/css/template.css';
+import ModalArtikel from "@/components/modal/ModalArtikel.vue";
+</script>
+
+<template>
+  <ModalArtikel/>
+
+  <div class="c1">
+    <h1 class="tit">Artikel Kami</h1>
+    <div class="can">
+      <div class="grid-con">
+        <router-link to="/read/slug" class="cover">
+          <img class="img" src="../../images/bsis.jpg" alt="">
+          <p>desc</p>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/article/ShowArticle.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '../../assets/css/viewartikel.css';
+import '../../assets/css/style.css';
+import '../../assets/css/template.css';
+</script>
+
+<template>
+  <div class="A1 col">
+    <div>
+      <h1 class="tit">Titleee</h1>
+      <br>
+      <img class="img gmbar" src="../../images/bsis.jpg">
+      <p style="margin-bottom: 1rem;">📷poto source </p>
+      <h4>oleh: author </h4>
+      <p style="margin-top: 5px;"> tnggal</p>
+      <p class="txt2">deskripsi</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
